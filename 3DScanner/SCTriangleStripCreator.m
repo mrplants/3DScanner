9e09d74c48536273320c4829090ce18fd27d4ae3
Shuyang Li <shuyang.li.95@gmail.com>
2014-01-18 10:42:09 -0500

    new cross product function

--- 3DScanner/SCTriangleStripCreator.m
+++ 3DScanner/SCTriangleStripCreator.m
@@ -7,6 +7,13 @@
 //
 
 #import "SCTriangleStripCreator.h"
+@import GLKit;
+
+@interface SCTriangleStripCreator ()
+
+GLKVector3 crossProductWithThreePoints(CGPoint3D pt1, CGPoint3D pt2, CGPoint3D pt3);
+
+@end
 
 @implementation SCTriangleStripCreator
 
@@ -28,7 +35,7 @@
 - (void)calculate {
     
     self.vertexArray = malloc(self.numberOfLinesGiven * self.lengthOfPointsOnLine * 6 * sizeof(GLfloat));
-    self.indexArray = malloc(self.numberOfLinesGiven * (self.lengthOfPointsOnLine + 2) * sizeof(GLuint));
+    self.indexArray = malloc((self.numberOfLinesGiven - 1) * (self.lengthOfPointsOnLine - 1) * 2 * 3 * sizeof(GLuint));
     
     for (int x = 0; x < self.numberOfLinesGiven; x++) {
         // doubling up the first point
@@ -37,6 +44,13 @@
         }
         // doubling up the last point
     }
+}
+
+GLKVector3 crossProductWithThreePoints(CGPoint3D root, CGPoint3D right, CGPoint3D left) {
+    GLKVector3 rightVector = GLKVector3Make(right.x - root.x, right.y - root.y, right.z - root.z);
+    GLKVector3 leftVector = GLKVector3Make(left.x - root.x, left.y - root.y, left.z - root.z);
+    
+    return GLKVector3CrossProduct(leftVector, rightVector);
 }
 
 @end
