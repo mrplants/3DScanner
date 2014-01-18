4e614f46e783137f9f3304cc17d37f8e302f682d
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2014-01-18 02:51:21 -0500

    Building the GLKVeiwController

---
+++ 3DScanner/ScannerGLKViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScannerGLKViewController.h
+//  3DScanner
+//
+//  Created by Sean Fitzgerald on 1/18/14.
+//  Copyright (c) 2014 Sean T Fitzgerald. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface ScannerGLKViewController : GLKViewController
+
+@end
