5a7de965dbaf871aa3ccf9d6bbbc2acefe225889
Shuyang Li <shuyang.li.95@gmail.com>
2014-01-17 22:33:26 -0500

    initial commit for height translator

---
+++ 3DScanner/SCHeightMapTranslator.m
@@ -0,0 +1,13 @@
+//
+//  SCHeightMapTranslator.m
+//  3DScanner
+//
+//  Created by Shuyang Li on 1/17/14.
+//  Copyright (c) 2014 Sean T Fitzgerald. All rights reserved.
+//
+
+#import "SCHeightMapTranslator.h"
+
+@implementation SCHeightMapTranslator
+
+@end
