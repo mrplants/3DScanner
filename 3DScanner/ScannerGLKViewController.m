e20139ffef0bf41987e994cb1121f29f8262edde
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2014-01-18 03:16:12 -0500

    Still working on GLKViewController

--- 3DScanner/ScannerGLKViewController.m
+++ 3DScanner/ScannerGLKViewController.m
@@ -8,25 +8,52 @@
 
 #import "ScannerGLKViewController.h"
 
-@interface ScannerGLKViewController ()
+@interface ScannerGLKViewController ()  {
+    float _curRed;
+    BOOL _increasing;
+}
 
 @end
 
 @implementation ScannerGLKViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    // Create an OpenGL ES context and assign it to the view loaded from storyboard
+    GLKView *view = (GLKView *)self.view;
+    view.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    // Set animation frame rate
+    self.preferredFramesPerSecond = 60;
+    
+    // load shaders, textures and vertex arrays, set up projection matrix
+//    [self setupGL];
+}
+
+- (void)update
+{
+    if (_increasing) {
+        _curRed += 1.0 * self.timeSinceLastUpdate;
+    } else {
+        _curRed -= 1.0 * self.timeSinceLastUpdate;
+    }
+    if (_curRed >= 1.0) {
+        _curRed = 1.0;
+        _increasing = NO;
+    }
+    if (_curRed <= 0.0) {
+        _curRed = 0.0;
+        _increasing = YES;
+    }
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    
+    glClearColor(_curRed, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +61,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
